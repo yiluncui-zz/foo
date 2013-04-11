958f1dc002eb53c2664bcf3ef6828f0044a1a936
Ned Wolpert <ned.wolpert@gmail.com>
2013-04-11 13:33:11 -0700

    Modified the private method uploadInMultipleParts() so each request to upload a part can recover from a network failure. Currently hardcoded to allow five retries per part.
    The last exception generated by attempting an upload will be rethrown if the upload failed all five tries.
    
    Does not add the part's checksum until after a successful upload, but will force a recalculation of the checksum on each try.
    
    No logging is done in the class, but if logging is added, it would be nice to log these retries.

--- src/main/java/com/amazonaws/services/glacier/transfer/ArchiveTransferManager.java
+++ src/main/java/com/amazonaws/services/glacier/transfer/ArchiveTransferManager.java
@@ -514,54 +514,66 @@ public class ArchiveTransferManager {
         String uploadId = initiateResult.getUploadId();
 
         try {
-            List<byte[]> binaryChecksums = new LinkedList<byte[]>();
+	        List<byte[]> binaryChecksums = new LinkedList<byte[]>();
 
-            long currentPosition = 0;
-            while (currentPosition < file.length()) {
-                long length = partSize;
-                if (currentPosition + partSize > file.length()) {
-                    length = file.length() - currentPosition;
+	        long currentPosition = 0;
+	        while (currentPosition < file.length()) {
+	            long length = partSize;
+	            if (currentPosition + partSize > file.length()) {
+	                length = file.length() - currentPosition;
+	            }
+
+                Exception failedException = null;
+                boolean completed = false;
+                int tries = 0;
+
+                while(!completed && tries<5){
+                    tries++;
+                    InputStream inputSubStream = newInputSubstream(file, currentPosition, length);
+                    inputSubStream.mark(-1);
+                    String checksum = TreeHashGenerator.calculateTreeHash(inputSubStream);
+                    byte[] binaryChecksum = BinaryUtils.fromHex(checksum);
+                    inputSubStream.reset();
+                    try {
+                        glacier.uploadMultipartPart(new UploadMultipartPartRequest()
+                                .withAccountId(accountId)
+                                .withChecksum(checksum)
+                                .withBody(inputSubStream)
+                                .withRange("bytes " + currentPosition + "-" + (currentPosition + length - 1) + "/*")
+                                .withUploadId(uploadId)
+                                .withVaultName(vaultName));
+                        completed = true;
+                        binaryChecksums.add(binaryChecksum);
+                    } catch (Exception e){
+                        failedException = e;
+                    } finally {
+                        try {inputSubStream.close();} catch (Exception e) {}
+                    }
+                }
+                if(!completed && failedException!=null){
+                    throw failedException;
                 }
 
-                InputStream inputSubStream = newInputSubstream(file, currentPosition, length);
-                inputSubStream.mark(-1);
-                String checksum = TreeHashGenerator.calculateTreeHash(inputSubStream);
-                byte[] binaryChecksum = BinaryUtils.fromHex(checksum);
-                binaryChecksums.add(binaryChecksum);
-                inputSubStream.reset();
+	            currentPosition += partSize;
+	        }
 
-                try {
-                    glacier.uploadMultipartPart(new UploadMultipartPartRequest()
-                        .withAccountId(accountId)
-                        .withChecksum(checksum)
-                        .withBody(inputSubStream)
-                        .withRange("bytes " + currentPosition + "-" + (currentPosition + length - 1) + "/*")
-                        .withUploadId(uploadId)
-                        .withVaultName(vaultName));
-                } finally {
-                    try {inputSubStream.close();} catch (Exception e) {}
-                }
+	        String checksum = TreeHashGenerator.calculateTreeHash(binaryChecksums);
 
-                currentPosition += partSize;
-            }
+	        String archiveSize = Long.toString(file.length());
+	        CompleteMultipartUploadResult completeMultipartUploadResult =
+	            glacier.completeMultipartUpload(new CompleteMultipartUploadRequest()
+	                .withAccountId(accountId)
+	                .withArchiveSize(archiveSize)
+	                .withVaultName(vaultName)
+	                .withChecksum(checksum)
+	                .withUploadId(uploadId));
 
-            String checksum = TreeHashGenerator.calculateTreeHash(binaryChecksums);
-
-            String archiveSize = Long.toString(file.length());
-            CompleteMultipartUploadResult completeMultipartUploadResult =
-                glacier.completeMultipartUpload(new CompleteMultipartUploadRequest()
-                    .withAccountId(accountId)
-                    .withArchiveSize(archiveSize)
-                    .withVaultName(vaultName)
-                    .withChecksum(checksum)
-                    .withUploadId(uploadId));
-
-            String artifactId = completeMultipartUploadResult.getArchiveId();
-            return new UploadResult(artifactId);
-        } catch (Exception e) {
-            glacier.abortMultipartUpload(new AbortMultipartUploadRequest(accountId, vaultName, uploadId));
-            throw new AmazonClientException("Unable to finish the upload", e);
-        }
+	        String artifactId = completeMultipartUploadResult.getArchiveId();
+	        return new UploadResult(artifactId);
+		} catch (Exception e) {
+			glacier.abortMultipartUpload(new AbortMultipartUploadRequest(accountId, vaultName, uploadId));
+			throw new AmazonClientException("Unable to finish the upload", e);
+		}
     }
 
 
